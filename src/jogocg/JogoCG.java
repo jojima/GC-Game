fc2885bb18d39189a06d4217e59fb4eac78975c4
Victor Otavio <victor_otavio205@Hotmail.com>
2017-01-04 23:24:29 -0200

    - Colision + animation bolt style

--- src/jogocg/JogoCG.java
+++ src/jogocg/JogoCG.java
@@ -1,98 +1,126 @@
 package jogoCG;
-import java.awt.EventQueue;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.Image;
+import java.awt.MediaTracker;
 import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
-import java.awt.TexturePaint;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import jogocg.kirby;
 
 
-class Surface extends JPanel {
-
-    private BufferedImage bg;
-    private TexturePaint bgtp;
+class Objects extends JPanel implements ActionListener, KeyListener{
+    private Timer animationTimer;
+    private Image background = new ImageIcon("background.png").getImage();
     private kirby player = new kirby();
-    private TexturePaint playertp;
+    int fase_y_max = 430, fase_x_max = 635;
+    int fase_y_min = 110, fase_x_min = 220;
+    //vetor de obstaculos
+    private Obstaculos obs = new Obstaculos();
+    int num_obstaculos;
     
-    public Surface() {
-
-        loadImages();
+    Objects(){
+        addKeyListener(this);
+        setFocusable(true);
     }
     
-    private void loadImages() {
+     boolean Colision(){
+        //caso passe alguma borda ou chegue em algum obstaculo
+        if(  player.getY() + player.getAltura() <= fase_y_min 
+          || player.getX() <= fase_x_min 
+          || player.getY() >= fase_y_max 
+          || player.getX() + player.getLargura() >= fase_x_max)
+            return true;
+        //para cada obstaculo
+        /*
+        if( (player.getY() + player.getAltura() <= obs.getY() && (player.getX()+player.getLargura() >= obs.getX() || player.getX() <= obs.getX() + obs.getLargura()))
+            || player.getX() <= obs.getX()+ obs.getLargura() && (player.getY()+player.getAltura() >= obs.getY() || player.getY() <= obs.getY() + obs.getAltura())
+            || player.getY() >= obs.getY() + obs.getAltura() && (player.getX()+player.getLargura() >= obs.getX() || player.getX() <= obs.getX() + obs.getLargura())
+            || player.getX() + player.getLargura() >= obs.getX() && (player.getY()+player.getAltura() >= obs.getY() || player.getY() <= obs.getY() + obs.getAltura()))
+            return true; */
+        if( (player.getY() + player.getAltura() <= 110 && (player.getX()+player.getLargura() >= 220 || player.getX() <= 250))
+            || player.getX() <= 250 && (player.getY()+player.getAltura() >= 110 || player.getY() <= 325)
+            || player.getY() >= 325 && (player.getX()+player.getLargura() >= 220 || player.getX() <= 250)
+            || player.getX() + player.getLargura() >= 220 && (player.getY()+player.getAltura() >= 110 || player.getY() <= 325))
+            return true;
+        return false;
+    }
+    
+    private void Cozinha(Graphics g) {
 
-        try {
+        g.drawImage(background,0,0,null);
+       
+        repaint();
+    }
+    public void startAnimation() {
+      if (animationTimer == null) {
+        player.setCurrentImage(0);
+        animationTimer = new Timer(0, this);
+        animationTimer.start();
+      } else if (!animationTimer.isRunning())
+        animationTimer.restart();
+    }
 
-            bg = ImageIO.read(new File("background.png"));            
-        } catch (IOException ex) {
-
-            Logger.getLogger(Surface.class.getName()).log(
-                    Level.SEVERE, null, ex);
+    public void stopAnimation() {
+      animationTimer.stop();
+    }
+    
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Cozinha(g);
+        if (player.getImage()[player.getCurrentImage()].getImageLoadStatus() == MediaTracker.COMPLETE) {
+            player.getImage()[player.getCurrentImage()].paintIcon(this, g, player.getX(), player.getY());
+            player.setCurrentImage((player.getCurrentImage() + 1) % 3);
         }
     }
 
-    private void doDrawing(Graphics g) {
-
-        Graphics2D g2d = (Graphics2D) g.create();
-
-        bgtp = new TexturePaint(bg, new Rectangle(0, 0, 690, 480));
-        playertp = new TexturePaint(player.getImage(), new Rectangle(0, 0, 36, 40));
-        
-        
-        g2d.setPaint(bgtp);
-        g2d.fillRect(0, 0, 690, 480);
-        
-        g2d.setPaint(playertp);
-        g2d.fillRect(player.getX(), player.getY(), 36, 40);
-        
-        g2d.dispose();
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-        super.paintComponent(g);
-        doDrawing(g);
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(Colision())
+            player.keyPressed(e);
+        
+        System.out.print("olar");
+        startAnimation();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+         player.keyReleased(e);
+         stopAnimation();
     }
 }
 
 public class JogoCG extends JFrame {
 
-    public JogoCG() {
+    JogoCG() {
 
-        initUI();
-    }
-    
-    private void initUI() {
-        
-        add(new Surface());
+        add(new Objects());
 
         setTitle("Jogo de CG MONSTRAO");
         setSize(700, 520);
         setLocationRelativeTo(null);        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
-
+   
     public static void main(String[] args) {
 
-        EventQueue.invokeLater(new Runnable() {
-        
-            @Override
-            public void run() {
-                JogoCG ex = new JogoCG();
-                ex.setVisible(true);
-            }
-        });
+        JogoCG novo = new JogoCG();
     }
 }
--- src/jogocg/kirby.java
+++ src/jogocg/kirby.java
@@ -6,6 +6,8 @@
 package jogocg;
 
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.awt.event.KeyEvent;
 import java.io.File;
@@ -14,39 +16,78 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 /**
  *
  * @author Yuela
  */
-public class kirby {
+public class kirby extends JPanel implements ActionListener {
     private int dx;
     private int dy;
     private int x;
     private int y;
-    private BufferedImage image;
+    public ImageIcon[] images,imagesF,imagesC,imagesD,imagesE;
+    private int animationDelay = 10,currentImage = 0,totalImages = 3;
+    private Timer animationTimer;
 
     public kirby() {
         
         initCraft();
     }
     
-    private void initCraft() {
-        try{
-            image = ImageIO.read(new File("k.png"));
-        
-        } catch (IOException ex) {
-
+    private void initCraft() {      
+      
+        imagesF = new ImageIcon[totalImages];
+        for (int i = 0; i < imagesF.length; ++i){
+            int j = i+1;
+            imagesF[i] = new ImageIcon("garcom/F" + j + ".png");
         }
+        imagesC = new ImageIcon[totalImages];
+        for (int i = 0; i < imagesC.length; ++i){
+            int j = i+1;
+            imagesC[i] = new ImageIcon("garcom/C" + j + ".png");
+        }
+        imagesD = new ImageIcon[totalImages];
+        for (int i = 0; i < imagesD.length; ++i){
+            int j = i+1;
+            imagesD[i] = new ImageIcon("garcom/D" + j + ".png");
+        }
+        imagesE = new ImageIcon[totalImages];
+        for (int i = 0; i < imagesE.length; ++i){
+            int j = i+1;
+            imagesE[i] = new ImageIcon("garcom/E" + j + ".png");
+        }
+        images = new ImageIcon[totalImages];
+        images = imagesF;
         
         x = 400;
         y = 300;        
     }
-
+    
+    public int getLargura() {
+        return 50;
+    }
+    
+    public int getAltura() {
+        return 50;
+    }
+    
+    public int getCurrentImage(){
+        return currentImage;
+    }
+    
+    public void setCurrentImage(int x){
+        currentImage = x;
+    }
+    
     public void move() {
         x += dx;
         y += dy;
     }
+    /*
 
+*/
     public int getX() {
         return x;
     }
@@ -55,8 +96,8 @@ public class kirby {
         return y;
     }
 
-    public BufferedImage getImage() {
-        return image;
+    public ImageIcon[] getImage() {
+        return images;
     }
 
     public void keyPressed(KeyEvent e) {
@@ -64,20 +105,26 @@ public class kirby {
         int key = e.getKeyCode();
 
         if (key == KeyEvent.VK_LEFT) {
-            dx = -1;
+            dx = -4;
+            images = imagesE;
         }
 
         if (key == KeyEvent.VK_RIGHT) {
-            dx = 1;
+            dx = 4;
+            images = imagesD;
         }
 
         if (key == KeyEvent.VK_UP) {
-            dy = -1;
+            dy = -4;
+            images = imagesC;
         }
 
         if (key == KeyEvent.VK_DOWN) {
-            dy = 1;
+            dy = 4;
+            images = imagesF;
         }
+       // startAnimation();
+        move();
     }
 
     public void keyReleased(KeyEvent e) {
@@ -99,5 +146,11 @@ public class kirby {
         if (key == KeyEvent.VK_DOWN) {
             dy = 0;
         }
+     //   stopAnimation();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
     }
 }
